a3e51739291a2d9bb96b34a2a4570578d244a24f
Fabian Santano <94444784+FabianSantano@users.noreply.github.com>
2024-04-07 19:42:54 -0700

    Add files via upload
    Added swagger frontend documentation , Fixed PUT routes for Dive and Expedition Table

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request
 import pyodbc
+from flask_swagger_ui import get_swaggerui_blueprint 
+
 
 app = Flask(__name__)
-SERVER = 'localhost'
-DATABASE = '<yourDatabaseName>'
-USERNAME = '<yourUsername>'
-PASSWORD = '<yourpassword>'
+SERVER = 'YOUR Server'
+DATABASE = 'YOUR DB'
+USERNAME = 'SA'
+PASSWORD = 'YOURPASSWO!'
 
-
+# Define the connection string
 connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};'
-from flask import request
 
-@app.route('/update/<int:expedition_id>/<int:expeditiondDataID_FK_id>', methods=['PUT'])
-def update_data(expedition_id, expeditiondDataID_FK_id):
-    try:
-        
-        data = request.json
-        
-        connection = pyodbc.connect(connectionString)
-      
-        cursor = connection.cursor()
-        
-        update_query = f"UPDATE Admin_BadStillImageURL SET HtmlError = ?, URL = ? WHERE ExpeditionID = ? AND ExpeditiondDataID_FK = ?"
-        cursor.execute(update_query, (data['HtmlError'], data['URL'], expedition_id, expeditiondDataID_FK_id))
-        
-        connection.commit()
-        cursor.close()
-        connection.close()
-        
-        return jsonify({'message': 'Data updated successfully'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
+
+
 
 @app.route('/get/<int:id>', methods=['GET'])
 def get_by_id(id):
@@ -51,6 +57,72 @@ def get_by_id(id):
             return str(row)
         else:
             return jsonify({'error': 'No entry matching this id'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/update/Expedition/<int:expedition_id>', methods=['PUT'])
+def updateExpedition_data(expedition_id):
+    try:
+        
+        data = request.json
+        connection = pyodbc.connect(connectionString)
+        cursor = connection.cursor()
+        columns = []
+        valueForColumn = []
+
+        #Go Through json body(data) and append column name and the column updated data
+        for colName, newValue in data.items():
+            # Same as DatetimeGMT = ?
+            columns.append(f"{colName} = ?")
+            valueForColumn.append(newValue)
+
+        valueForColumn.append(expedition_id)
+        columnsToUpdate = ","
+        columnsToUpdate = columnsToUpdate.join(columns)
+
+        # Construct the update query dynamically
+        update_query = f"UPDATE Expedition SET {columnsToUpdate} WHERE ExpeditionID = ?"
+
+        cursor.execute(update_query, valueForColumn)
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({'message': 'Data updated successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/update/Dive/<int:dive_id>', methods=['PUT'])
+def updateDive_data(dive_id):
+    try:
+        
+        data = request.json
+        connection = pyodbc.connect(connectionString)
+        cursor = connection.cursor()
+        columns = []
+        valueForColumn = []
+
+        #Go Through json body(data) and append column name and the column updated data
+        for colName, newValue in data.items():
+            #Same as DatetimeGMT = ?
+            columns.append(f"{colName} = ?")
+            valueForColumn.append(newValue)
+
+        valueForColumn.append(dive_id)
+        columnsToUpdate = ","
+        columnsToUpdate = columnsToUpdate.join(columns)
+
+        update_query = f"UPDATE Dive SET {columnsToUpdate} WHERE DiveID = ?"
+
+        cursor.execute(update_query, valueForColumn)
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        
+        return jsonify({'message': 'Data updated successfully'}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
