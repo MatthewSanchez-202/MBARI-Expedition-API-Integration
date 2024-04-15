91ede5803296bf8e59c75d103075e81124d54d3e
RyRom <109193666+RyRom@users.noreply.github.com>
2024-04-15 11:23:30 -0700

    Added tests for both get by id

--- tests.py
+++ tests.py
@@ -54,3 +54,17 @@ def test_create_dive():
 
     r = requests.post(f'{URL}/post/create_dive', json=data)
     assert r.status_code == 200
+
+def test_get_by_id_dive():
+    id = 2
+    r = requests.get(f'{URL}/getDive/{id}')
+    response = r.json()
+    assert(r.status_code == 200)
+    assert(response[0]['RovName'] == 'ROV2')
+    
+def test_get_by_id_expedition():
+    id = 1001
+    r = requests.get(f'{URL}/getExpedition/{id}')
+    response = r.json()
+    assert(r.status_code == 200)
+    assert(response[0]['ShipName'] == 'Ship2')
